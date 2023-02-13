6598a9a2410c4579a251079fd6256e3660cb1688
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-13 23:32:56 +0500

    array method custom reduce

---
+++ array-custom-method/array.js
@@ -0,0 +1,11 @@
+// Array.prototype.myReduce = function(old,cur){
+//  let sum = cur == undefined ? 0 :cur
+ 
+//  for(let i = cur == undefined ? 0 : cur;i<this.length;i++){
+//     sum = old(sum,this[i])
+//  }
+ 
+//  return sum
+// }
+
+// console.log([1,2,3,4].myReduce((a,b)=>b+a,0))
