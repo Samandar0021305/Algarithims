7a8e6d60319864428a9998037bec2f93052af0bb
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-14 00:33:18 +0500

    liner search algarihtm

---
+++ liner-search/index.js
@@ -0,0 +1,12 @@
+// function linerSearch(arr,target){
+//  for(let i = 0;i<arr.length;i++){{
+//     if(arr[i] === target){
+//         return i
+//     }
+//  }
+//  }  
+//  return -1
+// }
+
+// console.log(linerSearch([1,2,3,4,90],4))
+
