e9be5f5ec855d62c4f2a70ec2011dc69ed64f772
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-21 10:59:20 +0500

    commit

---
+++ recursion/index.js
@@ -0,0 +1,30 @@
+function CountDown(n){
+    if(n==0){
+        return "All"
+    }
+   console.log(n)
+   n--
+   return CountDown(n)
+}
+
+// console.log(CountDown(5));
+
+// n gacha bo'lgan sonni qo'shish
+function sumaRange(n){
+    if(n===1) return 1
+    return n + sumaRange(n-1)
+}
+
+// console.log(sumaRange(10))
+
+
+// sonni faktariolini topish
+
+function faktarial(n){
+    if(n==1) return 1
+    return n*faktarial(n-1)
+}
+
+// console.log(faktarial(5))
+
+
