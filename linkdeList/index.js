22fd7a770e8e0ea24fce47a57ade5b0888122f79
Samandar0021305 <TSamandardev89@gmail.com>
2023-03-08 20:08:50 +0500

    linked list

--- linkdeList/index.js
+++ linkdeList/index.js
@@ -1,9 +1,10 @@
-class Node {
+class Node{
     constructor(val){
         this.val = val
         this.next = null
     }
 }
+
 
 class LinkedList{
     constructor(){
@@ -11,22 +12,34 @@ class LinkedList{
         this.tail = null
         this.length = 0
     }
+
     push(x){
-      var newNode = new Node(x)
-      if(!this.head){
-        this.head = newNode;
-        this.tail = this.head
-      }else{
-        this.tail.next = newNode
-        this.tail = newNode
-      }
-      this.length++
-      return this
+        let newNode= new Node(x)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        }else{
+            this.tail.next = newNode
+             this.tail=newNode
+        }
+        this.length ++
+        return this
+    }
+    // traverse(){
+    //     var current= this.head
+    //     while(current){
+    //         console.log(current.val);
+    //         current = current.next
+    //     }
+    // }
+
+    pop(){
+        if(!this.head) return undefined
     }
 }
 
-
 let list = new LinkedList()
-list.push("hi")
-list.push("you")
-console.log(list)
+list.push("hhhhh")
+list.push("asdas")
+// list.traverse()
+console.log(list);
